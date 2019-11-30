0259e374d55c222b59b493c37b9cb3fd8f1c8ef5
Harish Karumuri <harishskarumuri@gmail.com>
2019-11-30 15:28:53 -0500

    [IN PROGRESS] merging category name with data

--- exploration.R
+++ exploration.R
@@ -7,5 +7,13 @@ us_trend_vids <- read_delim("data/USvideos.csv",
 
 ##Connect Category with Number
 library(rjson)
-us_category_id = fromJSON("data/US_category_id.json")
+US_category_id = fromJSON(file = "data/US_category_id.json")
+
+US_cateogry_id1 = as.data.frame(US_category_id$items) %>% 
+  gather("key", "val")
+  a = as.data.frame(str_subset(US_cateogry_id1$key, "^(id|snippet.title)")) %>% 
+        rename(key = `str_subset(US_cateogry_id1$key, "^(id|snippet.title)")`)
+
+b = left_join(a, US_cateogry_id1, by = "key")
+
 
