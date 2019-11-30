d4e43e40e80589e29b3df0355156cc2ad582b13b
Harish Karumuri <harishskarumuri@gmail.com>
2019-11-30 14:47:28 -0500

    [ADD] isolate category id and name

--- exploration.R
+++ exploration.R
@@ -3,5 +3,9 @@ library(tidyverse)
 us_trend_vids <- read_delim("data/USvideos.csv", 
                             delim = ",",
                             col_types = cols(trending_date = col_date(format = "%y.%d.%m")))
+#view(problems(us_trend_vids)) Data seems to be read fine, problems coming from vertical bar (|) in tags
 
-view(problems(us_trend_vids))
+##Connect Category with Number
+library(rjson)
+us_category_id = fromJSON("data/US_category_id.json")
+
