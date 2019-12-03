737f7c1efb4433acbb2879890c244af1ce5b7ca2
Harish Karumuri <harishskarumuri@gmail.com>
2019-12-03 12:18:06 -0500

    [ADD] Talk shows trending amount vs average views

---
+++ final_proj_try2.R
@@ -0,0 +1,43 @@
+#Import USdata set
+library(readr)
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+USvideos <- read_csv("data/USvideos.csv")
+#View(USvideos)
+
+talk_show_list = c("Jimmy Kimmel Live",
+                   "The Tonight Show Starring Jimmy Fallon",
+                   "TheEllenShow",
+                   "The Late Late Show with James Corden",
+                   "Team Coco",
+                   "The Late Show with Stephen Colbert",
+                   "Late Night with Seth Meyers")
+
+USvideos = USvideos %>% mutate(isTalkShow = channel_title %in% c("Jimmy Kimmel Live",
+                                                      "The Tonight Show Starring Jimmy Fallon",
+                                                      "TheEllenShow",
+                                                      "The Late Late Show with James Corden",
+                                                      "Team Coco",
+                                                      "The Late Show with Stephen Colbert",
+                                                      "Late Night with Seth Meyers"))
+
+
+#check amount of average views each top featured channel gets per video
+topChannelsAverageTrending= USvideos %>% group_by(channel_title) %>% summarise(TrendingAmount = n(), average = sum(views)/n()) %>%
+    arrange(desc(TrendingAmount)) %>% top_n(150,TrendingAmount) %>% glimpse()
+#top thirty channels based on trending amount not by average views
+
+top = as.data.frame(topChannelsAverageTrending) %>% mutate(isTalkShow = channel_title %in% c("Jimmy Kimmel Live",
+                                                                                       "The Tonight Show Starring Jimmy Fallon",
+                                                                                       "TheEllenShow",
+                                                                                       "The Late Late Show with James Corden",
+                                                                                       "Team Coco",
+                                                                                       "The Late Show with Stephen Colbert",
+                                                                                       "Late Night with Seth Meyers"))
+
+ggplot(top,aes(TrendingAmount, average, color = isTalkShow)) +
+  geom_point() #+ 
+  #geom_text(aes(label = ifelse(isTalkShow, channel_title, "")), size = 2)
+#for talkshows: the graph shows that there is no correlation between average views and amount of times trending
+
